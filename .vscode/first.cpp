76294f20271d74a187a86595c30064785521657e
NamanJain62 <20CBS1062@cuchd.in>
2023-10-09 19:06:57 +0530

    first commit

---
+++ .vscode/first.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a,b;
+    cout<<"enter a is: ";
+    cin>>a;
+    cout<<"Enter b is: ";
+    cin>>b;
+    cout<<"A+B is: "<<a+b;
+}
---
+++ .vscode/vect.cpp
@@ -0,0 +1,6 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void explainPair(){
+    pair<int,int> p = {1,2};
+}
